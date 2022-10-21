65938e4d4a1d0570641a30b87a40f86e35da425f
GeovanaNoal <116393239+GeovanaNoal@users.noreply.github.com>
2022-10-21 19:46:04 -0300

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -1,5 +1,31 @@
 import streamlit as st
 
+from sklearn.ensemble import RandomForestClassifier 
+import pandas as pd
+dados = pd.read_csv('/content/drive/MyDrive/Inteligência Artificial - Geovana/23-09 pesq. cursos - Geovana/Floresta Randômica/Iris_Floresta_Randomica.csv')
+
+classes = dados['Species']
+nomesColunas = dados.columns.to_list()
+tamanho = len(nomesColunas)
+nomesColunas = nomesColunas[1:tamanho-1]
+features = dados[nomesColunas]
+
+from sklearn.model_selection import train_test_split
+
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
+                                                                               classes,
+                                                                               test_size=0.25,
+                                                                               random_state=2)
+
+floresta = RandomForestClassifier(n_estimators=90) 
+
+floresta.fit(features_treino,classes_treino)
+predicoes = floresta.predict(features_teste)
+from sklearn import metrics 
+print(metrics.classification_report(classes_teste,predicoes))
+
+
+
 st.title('Aplicativo de IA')
 nome = st.text_input('Digite o seu nome:')
 if st.button('Clique aqui'):
